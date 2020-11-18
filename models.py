136704fc636e44ebb65a17924ab5faae3a40fc27
d0dders <dodders@outlook.com>
2020-11-18 20:10:20 +0000

    Added hashing and create user method

--- models.py
+++ models.py
@@ -1,15 +1,30 @@
 import datetime
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
 from peewee import *
 
 DATABASE = SqliteDatabase('social.db')
 
-class User(Model):
+class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=100)
-    joined_at = (DateTimeField=datetime.datetime.now)
+    joined_at = DateTimeField(default=datetime.datetime.now)
     is_admin = BooleanField(default=False)
 
     class Meta:
         database = DATABASE
         order_by = ('-joined_at')
+
+    
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin
+            )
+        except IntegrityError:
+            raise ValueError("User already exists")

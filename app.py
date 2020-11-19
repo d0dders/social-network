5ddf7814f10cc264d7e1139fe498647c5a89024e
d0dders <dodders@outlook.com>
2020-11-19 22:13:40 +0000

    Start app.py with some db setup and loginmanager

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, g
+from flask_login import LoginManager
+
+import models
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
+
+app = Flask(__name__)
+app.secret_key = 'fasfasfnksafkjsdafnlsdknflksjflasb'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return model.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+@app.before_request
+def before_request():
+    """Connect to the db before each request"""
+    g.db = models.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    """Close the db connection after each request"""
+    g.db.close()
+    return response
+
+
+if __name__ == "__main__":
+    models.initialize()
+    models.User.create_user(
+        name='dodders',
+        email='dodders@email.com',
+        password='password',
+        admin=True
+        )
+    app.run(debug=DEBUG, host=HOST, port=PORT)

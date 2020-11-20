228a32741498fa42d2537f99ce791e6a88187b98
d0dders <dodders@outlook.com>
2020-11-20 23:19:37 +0000

    Add register and login views and templates

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, g
-from flask_login import LoginManager
+from flask import (Flask, g, render_template, flash, redirect,
+                    url_for)
+from flask_bcrypt import check_password_hash
+from flask_login import (LoginManager, login_user, logout_user, 
+                            login_required)
 
+import forms
 import models
 
 DEBUG = True
@@ -36,12 +40,60 @@ def after_request(response):
     return response
 
 
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash("Yay, you registered!", "success")
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data 
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            flash("Your email or password doesn't match", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You've been logged in!", "success")
+                return redirect(url_for('index'))
+            else:             
+                flash("Your email or password doesn't match", "error")
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You've been logged out. Come back soon!", "success")
+    return redirect(url_for('index'))
+
+
+@app.route('/')
+def index():
+    return 'Hey'
+
+
 if __name__ == "__main__":
     models.initialize()
-    models.User.create_user(
-        name='dodders',
-        email='dodders@email.com',
-        password='password',
-        admin=True
-        )
+    try:
+        models.User.create_user(
+            username='dodders',
+            email='dodders@email.com',
+            password='password',
+            admin=True
+            )
+    except ValueError:
+        pass
     app.run(debug=DEBUG, host=HOST, port=PORT)

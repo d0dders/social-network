19e46842f95d13729552c7cc191dfb45a1b2ab93
d0dders <dodders@outlook.com>
2020-11-22 19:12:44 +0000

    Add post form and template

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import (DataRequired, Regexp, ValidationError, Email, Length, EqualTo)
 from models import User
 
@@ -53,3 +53,6 @@ class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     
+
+class PostForm(Form):
+    content = TextAreaField("What's Up?", validators=[DataRequired()])
